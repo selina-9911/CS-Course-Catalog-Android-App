392b5337c1b29e262f70b762b674ea8f233f00e5
selina-9911 <qinghao2@illinois.edu>
2021-04-21 22:50:18 -0500

    made changes to client.java and rating.java to get started with the test suite

---
+++ app/src/main/java/edu/illinois/cs/cs125/spring2021/mp/models/Rating.java
@@ -0,0 +1,16 @@
+package edu.illinois.cs.cs125.spring2021.mp.models;
+
+public class Rating {
+    public static final double NOT_RATED = -1.0;
+
+    public Rating(String setID, double setRating) {
+
+    }
+
+    public String getId() {
+        return null;
+    }
+    public double getRating() {
+        return NOT_RATED;
+    }
+}
